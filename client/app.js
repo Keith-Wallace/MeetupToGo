52ef4678bd55bc4f7fc0c645d781e7014c1ba350
Keith Wallace <keith.t.wallace@gmail.com>
2016-08-23 19:40:49 -0400

    Code cleanup and documentation

--- client/app.js
+++ client/app.js
@@ -1,8 +1,14 @@
+/*
+    SET UP OUR FRONT-END FRAMEWORK
+*/
+
 angular.module('MeetUpAPI', [])
 
+// SET UP OUR ANGULAR CONTROLLER WITH FUNCTION HANDLERS
 .controller('CategoryController', function($scope, $http) {
   $scope.groupTable = true;
 
+  // FROM OUR HOMEPAGE, PULL CATEGORY DATA FROM API TO DROP-DOWN MENU
   $http.get('/api/categories')
     .success(function(data) {
       $scope.category = data;
@@ -11,7 +17,9 @@ angular.module('MeetUpAPI', [])
       console.log('Error: ' + data);
     });
 
+    // CLICK EVENT LISTENER - CALLS FUNCTION WHEN SEARCH IS SUBMITTED
     $scope.findGroup = function(zipcode, category) {
+      // ERROR HANDLING BEFORE SENDING A GET REQUEST TO API
       if (zipcode === undefined && category === undefined) {
         alert('Please select a category and enter zip code');
       } else if (category === undefined) {
@@ -21,6 +29,8 @@ angular.module('MeetUpAPI', [])
       } else if ( zipcode.match(/[a-z]/i) ) {
         alert('Please enter a valid zip code');
       } else {
+        // VALIDATION PASSES
+        // SEND REQUIRED SEACH PARAMATERS FOR API
         $http.get('/api/groups', {params: {category_id: $scope.selected.id, zip: $scope.zipcode}})
           .success(function(data) {
               $scope.groups = data;
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+/*
+    SET UP OUR SERVER CONFIG
+*/
+
+// DEFINE DEPENDENCIES/MODULES
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var request = require('request');
 var express = require('express');
 var app = express();
 
+// SET STATIC FILE LOCATION & PARSING
 app.use(express.static(__dirname + '/client')); 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// ROUTES
+// CONFIGURE ROUTES TO API CALLS
 app.get('/api/categories', function(req, res) {
   var urlAPI = 'https://api.meetup.com/2/categories?key=4e47134a736d2f51696c4b8653b684c&sign=true';
  
@@ -27,16 +33,16 @@ app.get('/api/groups', function(req, res) {
     if (!error && response.statusCode == 200) {
       res.send(body);
     } else {
-      // console.log('====> ', body[details])
       console.log('ERROR: ', error);
     }
   });
 });
 
-// APP
+// LOAD OUR SINGLE PAGE APPLICATION
 app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
 
+// LISTEN FOR ANY REQUESTS TO OUR SERVER
 app.listen(process.env.PORT || 8000);
 console.log("App listening on port 8080");
